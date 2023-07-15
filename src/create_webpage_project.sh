32f0a5d6a3830c4640f27cd29fbb3707efb9b9b2
Pawel Kusinski <pawel.kusinski@gmail.com>
2023-07-15 11:27:50 +0400

    Add create_webpage_project.sh

--- src/create_webpage_project.sh
+++ src/create_webpage_project.sh
@@ -22,6 +22,28 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-alias cdp='cd ~/projects'
-alias ccp='~/projects/shell_scripts/src/create_c_project.sh "$@"'
-alias cwpp='~/projects/shell_scripts/src/create_webpage_project.sh "$@"'
+# Get the project name from the command-line argument or use "my_webpage_project" as default
+project_name="${1:-my_webpage_project}"
+
+mkdir "$project_name"
+cd "$project_name"
+mkdir .vscode
+mkdir src
+
+cat > src/index.html <<EOF
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>$project_name</title>
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <h1>$project_name</h1>
+  </body>
+</html>
+EOF
+
+touch src/style.css
+touch src/script.js
+code .
+exit 0
